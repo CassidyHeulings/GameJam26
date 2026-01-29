44a4fab010a4a541f3e27e14514da8c4104a8917
CassidyHeulings <cassjheuls@gmail.com>
2026-01-29 14:46:42 -0500

    Added outline for game loop

--- Source Files/main.cpp
+++ Source Files/main.cpp
@@ -6,6 +6,7 @@
 #include "../Header Files/Player.hpp"
 
 using namespace sf;
+using namespace std;
 
 int main() {
     // initialize game states
@@ -19,15 +20,30 @@ int main() {
     RenderWindow window(VideoMode(resolution.x, resolution.y), "Mask", Style::Fullscreen);
     View mainView(FloatRect(0,0, resolution.x, resolution.y));
 
+    // vertex array to hold all graphics
+    VertexArray canvas(Quads, 0);
+    // TODO fill these in when classes are created
+    // dispatch events to objects
+    // vector to hold game objects
+    // factory to construct game objects
+    // send game elements to factory to set up game
+
     // initialize game variables
     Vector2f mouseWorldPosition;
     Vector2i mouseScreenPosition;
-
+    Clock clock;
     IntRect world;
 
     // game loop
     while (window.isOpen()) {
-
+        float dt = clock.restart().asSeconds(); // time per frame
+        // TODO fill these in when classes are made
+        // dispatch input to objects
+        window.clear(); // clear frame
+        // update game objects
+        // draw game objects to canvas
+        window.display(); // show next frame
     }
+
     return 0;
 }
