73a19254efabc8ab8bb382884a20c3f055adeae9
CassidyHeulings <cassjheuls@gmail.com>
2026-01-29 14:18:55 -0500

    Starting main

--- Source Files/main.cpp
+++ Source Files/main.cpp
@@ -1,3 +1,30 @@
 //
 // Created by Cassidy Heulings on 1/27/26.
 //
+
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+int main() {
+    // initialize game states
+    enum class State { GAME_OVER, CRAFTING, PLAYING };
+    State state = State::GAME_OVER; // start game in game over state
+
+    // create window with screen resolution
+    Vector2f resolution;
+    resolution.x = VideoMode::getDesktopMode().width;
+    resolution.y = VideoMode::getDesktopMode().height;
+    RenderWindow window(VideoMode(resolution.x, resolution.y), "Mask", Style::Fullscreen);
+    View mainView(FloatRect(0,0, resolution.x, resolution.y));
+
+    // initialize game variables
+    Vector2f mouseWorldPosition;
+    Vector2i mouseScreenPosition;
+    IntRect world;
+
+    // game loop
+    while (window.isOpen()) {
+
+    }
+    return 0;
+}
